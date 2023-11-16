856c47dd33a15a1acee920c73128715c37cde302
Salvatore de Benedictis <s.debenedictis12@studenti.uniba.it>
2023-11-16 19:48:20 +0100

    Refactoring on different files

--- src/models/clustering.py
+++ src/models/clustering.py
@@ -10,41 +10,44 @@ import conf  # noqa:E402
 # pylint: enable=wrong-import-position
 
 
-print("Starting to cluster out the data...")
-train_tracks_df = pd.read_csv(conf.PRO_TRAIN_SET_PATH)
-test_tracks_df = pd.read_csv(conf.PRO_TEST_SET_PATH)
+def clustering():
+    print("Starting to cluster out the data...")
+    train_tracks_df = pd.read_csv(conf.PRO_TRAIN_SET_PATH)
+    test_tracks_df = pd.read_csv(conf.PRO_TEST_SET_PATH)
 
-train_set = train_tracks_df[conf.FEATURES]
-test_set = test_tracks_df[conf.FEATURES]
+    train_set = train_tracks_df[conf.FEATURES]
+    test_set = test_tracks_df[conf.FEATURES]
 
 # Clustering train set (user's preferences playlist)
-kmedoids = KMedoids(n_clusters=conf.NO_CLUSTER,
+    kmedoids = KMedoids(n_clusters=conf.NO_CLUSTER,
                     random_state=conf.RND_STATE)
 
-clusters = kmedoids.fit_predict(train_set)
-train_tracks_df['Cluster'] = clusters
+    clusters = kmedoids.fit_predict(train_set)
+    train_tracks_df['Cluster'] = clusters
 
 # Cluster the test set using the trained model for song suggestions.
-test_clusters = kmedoids.predict(test_set)
-test_tracks_df['Cluster'] = test_clusters
+    test_clusters = kmedoids.predict(test_set)
+    test_tracks_df['Cluster'] = test_clusters
 
-print("Clustering completed!")
+    print("Clustering completed!")
 
-print("===================================================")
-OUTPUT_TRAIN_FILE = conf.OUTPUT_DIR + 'clustertrainSet.csv'
-OUTPUT_TEST_FILE = conf.OUTPUT_DIR + 'clustertestSet.csv'
+    print("===================================================")
+    OUTPUT_TRAIN_FILE = conf.OUTPUT_DIR + 'clustertrainSet.csv'
+    OUTPUT_TEST_FILE = conf.OUTPUT_DIR + 'clustertestSet.csv'
 
-print("Storing the processed files in the folder processed_data.")
-train_tracks_df.to_csv(OUTPUT_TRAIN_FILE, index=False)
-test_tracks_df.to_csv(OUTPUT_TEST_FILE, index=False)
-print("The data has been stored!")
-print("===================================================")
+    print("Storing the processed files in the folder processed_data.")
+    train_tracks_df.to_csv(OUTPUT_TRAIN_FILE, index=False)
+    test_tracks_df.to_csv(OUTPUT_TEST_FILE, index=False)
+    print("The data has been stored!")
+    print("===================================================")
 
-print("First 5 rows of the train set:")
-print(train_tracks_df.head())
+    print("First 5 rows of the train set:")
+    print(train_tracks_df.head())
 
-print("First 5 rows of the test set:")
-print(test_tracks_df.head())
+    print("First 5 rows of the test set:")
+    print(test_tracks_df.head())
 
 # Save the model to the file
-dump(kmedoids, conf.MODEL_FILE_PATH)
+    dump(kmedoids, conf.MODEL_FILE_PATH)
+
+clustering()
--- src/models/metrics.py
+++ src/models/metrics.py
@@ -11,51 +11,57 @@ from joblib import load  # noqa:E402
 import conf  # noqa:E402
 # pylint: enable=wrong-import-position
 
-rec_songs_pred = pd.read_csv(conf.RECOMMENDATIONS_PATH)
-rec_songs_pred['Feedback'] = 1
+def metrics():
+    rec_songs_pred = pd.read_csv(conf.RECOMMENDATIONS_PATH)
+    rec_songs_pred['Feedback'] = 1
 
-print("These are the systems recommendations:")
-print(rec_songs_pred)
-print("Comparison with user-liked songs in the test set")
+    print("These are the systems recommendations:")
+    print(rec_songs_pred)
+    print("Comparison with user-liked songs in the test set")
 
-fdbk_1 = pd.read_csv(conf.FEEDBACKUSER1_PATH)
-fdbk_2 = pd.read_csv(conf.FEEDBACKUSER2_PATH)
+    fdbk_1 = pd.read_csv(conf.FEEDBACKUSER1_PATH)
+    fdbk_2 = pd.read_csv(conf.FEEDBACKUSER2_PATH)
 
-CORRECT_PREDICTIONS = 0
+    CORRECT_PREDICTIONS = 0
 
-for _, row_pred in rec_songs_pred.iterrows():
+    for _, row_pred in rec_songs_pred.iterrows():
+        song_name = row_pred['Name']
+        artist_name = row_pred['Artist']
+        label = row_pred['Feedback']
 
-    song_name = row_pred['Name']
-    artist_name = row_pred['Artist']
-    label = row_pred['Feedback']
+        for _, row_fbk in fdbk_1.iterrows():
+            song_name_fb = row_fbk['Name']
+            artist_name_fb = row_fbk['Artist']
+            label_fb = row_fbk['Feedback']
 
-    for _, row_fbk in fdbk_1.iterrows():
+            if song_name == song_name_fb and artist_name == artist_name_fb:
+                if label == label_fb:
+                    CORRECT_PREDICTIONS += 1
 
-        song_name_fb = row_fbk['Name']
-        artist_name_fb = row_fbk['Artist']
-        label_fb = row_fbk['Feedback']
+    accuracy = float(CORRECT_PREDICTIONS / conf.NO_RECOMMENDATIONS)
 
-        if song_name == song_name_fb and artist_name == artist_name_fb:
+    print(f"Accuracy: {accuracy:.2f}")
+    
+    return accuracy
 
-            if label == label_fb:
-                CORRECT_PREDICTIONS += 1
 
-accuracy = float(CORRECT_PREDICTIONS / conf.NO_RECOMMENDATIONS)
+def log_experiments(accuracy):
+    kmedoids = load(conf.MODEL_FILE_PATH)
 
-print(f"Accuracy: {accuracy:.2f}")
+    dagshub.init("MusicExpress", "se4ai2324-uniba", mlflow=True)
 
-kmedoids = load(conf.MODEL_FILE_PATH)
+    mlflow.start_run()
 
-dagshub.init("MusicExpress", "se4ai2324-uniba", mlflow=True)
-
-mlflow.start_run()
-
-mlflow.sklearn.log_model(kmedoids, "kmedoids-model")
-mlflow.log_params({
+    mlflow.sklearn.log_model(kmedoids, "kmedoids-model")
+    mlflow.log_params({
     "no_cluster": conf.NO_CLUSTER,
     "rnd_state": conf.RND_STATE,
     'no_recommendations': conf.NO_RECOMMENDATIONS
 })
-mlflow.log_metric("Accuracy", accuracy)
+    mlflow.log_metric("Accuracy", accuracy)
 
-mlflow.end_run()
+    mlflow.end_run()
+
+
+accuracy = metrics()
+log_experiments(accuracy)
