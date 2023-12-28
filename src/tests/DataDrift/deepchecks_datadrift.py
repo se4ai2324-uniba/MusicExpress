af83f961385b49e59cb7e24529155d6375d7892b
Salvatore de Benedictis <s.debenedictis12@studenti.uniba.it>
2023-12-28 18:57:13 +0100

    Added data drift github action

---
+++ src/tests/DataDrift/deepchecks_datadrift.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import os
+import sys
+import pandas as pd
+from deepchecks.tabular.checks import FeatureDrift
+from deepchecks.tabular.checks import MultivariateDrift
+from deepchecks.tabular import Dataset
+current_dir = os.getcwd()
+src = os.path.dirname(os.path.dirname(current_dir))
+sys.path.insert(0, src)
+import conf
+# Dataset 
+root = os.path.dirname(os.path.dirname(os.path.dirname(current_dir)))
+sys.path.insert(0, root)
+processed_train_data_directory = root + '/'+ conf.PRO_DATA_DIR + 'trainSet.csv'
+processed_test_data_directory = root + '/'+ conf.PRO_DATA_DIR + 'testSet.csv'
+# Numerical Feature Drift
+df_processed_train = pd.read_csv(processed_train_data_directory)
+df_processed_train = df_processed_train.drop(['Artist', 'Name'], axis=1)
+df_processed_test = pd.read_csv(processed_test_data_directory)
+df_processed_test = df_processed_test.drop(['Artist', 'Name'], axis=1)
+ds_processed_train = Dataset(df_processed_train)
+ds_processed_test = Dataset(df_processed_test)
+# Univariate Drift
+check = FeatureDrift()
+uni_result = check.run(train_dataset=ds_processed_train, test_dataset=ds_processed_test)
+report_path = root + '/reports/deepchecks/'
+uni_result.save_as_html(report_path+'Drift_Univariate_report.html')
+print('Univariate Drift report saved in reports/deepchecks folder')
+# Multivariate Drift
+check = MultivariateDrift()
+multi_result = check.run(train_dataset=ds_processed_train, test_dataset=ds_processed_test)
+multi_result.save_as_html(report_path+'Drift_Multivariate_report.html')
+print('Multivariate Drift report saved in reports/deepchecks folder')
