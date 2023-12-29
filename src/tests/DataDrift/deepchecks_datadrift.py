29289817bfbe74667bd60ca3c069d4a01dfc3fd7
Salvatore de Benedictis <s.debenedictis12@studenti.uniba.it>
2023-12-29 16:58:14 +0100

    updated deepchecks_datadrift.py

--- src/tests/DataDrift/deepchecks_datadrift.py
+++ src/tests/DataDrift/deepchecks_datadrift.py
@@ -1,6 +1,7 @@
 """Script to check data drift using deepchecks library"""
 
 # pylint: disable=wrong-import-position
+import os                                                   # noqa:E402
 import sys                                                  # noqa:E402
 sys.path.append('src')                                      # noqa:E402
 import pandas as pd                                         # noqa:E402
@@ -10,6 +11,13 @@ from deepchecks.tabular import Dataset                      # noqa:E402
 import conf                                                 # noqa:E402
 # pylint: enable=wrong-import-position
 
+# Define the report file path
+report_file_path_univariate = os.path.join(
+    conf.DEEPCHECKS_REPORT_DIR, 'Drift_Univariate_report.html'
+    )
+report_file_path_multivariate = os.path.join(
+    conf.DEEPCHECKS_REPORT_DIR, 'Drift_Multivariate_report.html'
+    )
 
 # Import Data
 train_data = pd.read_csv(conf.PRO_TRAIN_SET_PATH)
@@ -27,13 +35,28 @@ check = FeatureDrift()
 uni_result = check.run(
     train_dataset=train_data_ds, test_dataset=test_data_ds
     )
-uni_result.save_as_html(conf.DEEPCHECKS_REPORT_DIR + 'Drift_Univariate_report.html')    # noqa:E501
-print('The Univariate Drift report has been saved in reports/deepchecks folder.')       # noqa:E501
 
+# Remove the file if it exists
+if os.path.exists(report_file_path_univariate):
+    os.remove(report_file_path_univariate)
+
+# Save the report
+uni_result.save_as_html(
+    conf.DEEPCHECKS_REPORT_DIR + 'Drift_Univariate_report.html'
+    )
+print('The Univariate Drift report has been saved in reports/deepchecks.')
 # Run Multivariate Drift
 check = MultivariateDrift()
 multi_result = check.run(
     train_dataset=train_data_ds, test_dataset=test_data_ds
     )
-multi_result.save_as_html(conf.DEEPCHECKS_REPORT_DIR + 'Drift_Multivariate_report.html') # noqa:E501
-print('The Multivariate Drift report has been saved in reports/deepchecks folder.')      # noqa:E501
+
+# Remove the file if it exists
+if os.path.exists(report_file_path_multivariate):
+    os.remove(report_file_path_multivariate)
+
+# Save the report
+multi_result.save_as_html(
+    conf.DEEPCHECKS_REPORT_DIR + 'Drift_Multivariate_report.html'
+    )
+print('The Multivariate Drift report has been saved in reports/deepchecks.')
