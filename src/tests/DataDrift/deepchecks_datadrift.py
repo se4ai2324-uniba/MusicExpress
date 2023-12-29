d8f14bc973220ce5a9ccf03ba15d98be6e71838c
Salvatore de Benedictis <s.debenedictis12@studenti.uniba.it>
2023-12-29 12:50:37 +0100

    added deepchecks to requirements.txt

--- src/tests/DataDrift/deepchecks_datadrift.py
+++ src/tests/DataDrift/deepchecks_datadrift.py
@@ -11,11 +11,13 @@ src = os.path.dirname(os.path.dirname(current_dir))
 sys.path.insert(0, src)
 import conf        # noqa:E402
 # pylint: enable=wrong-import-position
+
 # Dataset
 root = os.path.dirname(os.path.dirname(os.path.dirname(current_dir)))
 sys.path.insert(0, root)
 processed_train_data_directory = root + '/' + conf.PRO_DATA_DIR + 'trainSet.csv'    # noqa:E501
 processed_test_data_directory = root + '/' + conf.PRO_DATA_DIR + 'testSet.csv'
+
 # Numerical Feature Drift
 df_processed_train = pd.read_csv(processed_train_data_directory)
 df_processed_train = df_processed_train.drop(['Artist', 'Name'], axis=1)
@@ -23,6 +25,7 @@ df_processed_test = pd.read_csv(processed_test_data_directory)
 df_processed_test = df_processed_test.drop(['Artist', 'Name'], axis=1)
 ds_processed_train = Dataset(df_processed_train)
 ds_processed_test = Dataset(df_processed_test)
+
 # Univariate Drift
 check = FeatureDrift()
 uni_result = check.run(
@@ -31,6 +34,7 @@ uni_result = check.run(
 report_path = root + '/reports/deepchecks/'
 uni_result.save_as_html(report_path+'Drift_Univariate_report.html')
 print('Univariate Drift report saved in reports/deepchecks folder')
+
 # Multivariate Drift
 check = MultivariateDrift()
 multi_result = check.run(
